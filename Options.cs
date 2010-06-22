3bbf3a2f6164537f67dfc11467fc4031fe133a9b
mashiro <y.mashiro@gmail.com>
2010-06-23 07:20:01 +0900

    CaseInsensitiveなDictionaryとして定義

---
+++ Options.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spica.Applications.TwitterIrcGateway.AddIns.GroongaLogger
+{
+	public class GroongaLoggerOptions : Dictionary<String, String>
+	{
+		public GroongaLoggerOptions()
+			: base(StringComparer.InvariantCultureIgnoreCase)
+		{
+		}
+
+		public GroongaLoggerOptions(GroongaLoggerOptions options)
+			: base(options, StringComparer.InvariantCultureIgnoreCase)
+		{
+		}
+	}
+}
